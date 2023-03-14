22b29b78a7871878bc0dedfbdbba9a6ad2a547c7
alamin-merndev <alaminnishat@gmail.com>
2023-03-14 17:40:52 +0600

    cors fixed

--- index.js
+++ index.js
@@ -1,9 +1,11 @@
 const express = require("express");
 const app = express();
+const cors = require("cors");
 const port = process.env.PORT || 5000;
 const mongoose = require("mongoose");
 
 // middleware
+app.use(cors());
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 
